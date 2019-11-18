fb6ca6cbfc4efca1554487453f6408ab781f264a
young-zy <youngzhaozhou@gmail.com>
2019-11-18 23:46:11 +0800

    Practice

--- src/LambdaPractice.kt
+++ src/LambdaPractice.kt
@@ -20,14 +20,56 @@ class Game {
         path.clear()
         false
     }
+
+    fun move(where: () -> Boolean) {
+//        where.invoke()
+        where()         //same??
+        //move location
+    }
+
+    fun makeMove(dir: String) {
+        move(
+            when (dir) {
+                "n" -> north
+                "s" -> south
+                "e" -> east
+                "w" -> west
+                else -> north
+            }
+        )
+
+    }
+
 }
 
 fun main() {
     val game = Game()
-    game.north
-    game.east
-    game.south
-    game.west
-    game.end
-    println(game.path)
+//    game.north
+//    game.east
+//    game.south
+//    game.west
+//    game.end
+//    println(game.path)
+    while (true) {
+        print("Enter a direction: n/s/e/w:")
+        game.makeMove(readLine() ?: "n")
+    }
+}
+
+
+class Location(val width: Int = 4, val height: Int = 4) {
+    val map = Array(width) { arrayOfNulls<String>(height) }
+
+    init {
+        for (i in 0..3) {
+            for (j in 0..3) {
+                map[i][j] = (i * 4 + j).toString()
+            }
+        }
+    }
+
+    fun updateLocation() {
+
+    }
+
 }
